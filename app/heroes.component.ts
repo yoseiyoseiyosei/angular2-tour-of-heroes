9f405a2f88bd2df342a707c145e0bdf71b319b3d
yoseiyoseiyosei <fairylandcompany@gmail.com>
2016-09-22 19:14:42 +0900

    酒精

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 import { Router } from '@angular/router';
